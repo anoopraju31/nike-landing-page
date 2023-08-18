52d7a344c813f906d1b6a8adc58e30a80d549702
anoopraju31 <anoopraju31@gmail.com>
2023-08-18 22:32:36 +0530

    Popular Products: styled Heading

--- src/app/sections/PopularProducts.tsx
+++ src/app/sections/PopularProducts.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import { products } from '@/utills/popularProducts'
 
 const PopularProducts = () => {
-	return <div>PopularProducts</div>
+	return (
+		<section className='padding'>
+			<div id='products' className='max-container max-sm:mt-12'>
+				{/* Heading */}
+				<div className='flex flex-col justify-start gap-5'>
+					<h2 className='text-4xl font-palanquin font-bold'>
+						Our
+						<span className='text-coral-red'> Popular </span>
+						Products
+					</h2>
+					<p className='lg:max-w-lg mt-2 font-montserrat text-slate-gray'>
+						Experience top-notch quality and style with our sought-after
+						selections. Discover a world of comfort, design, and value
+					</p>
+				</div>
+
+				{/* Products */}
+				<div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14'></div>
+			</div>
+		</section>
+	)
 }
 
 export default PopularProducts
---
+++ src/utills/popularProducts.tsx
@@ -0,0 +1,22 @@
+export const products = [
+	{
+		imgURL: '/images/shoe4.svg',
+		name: 'Nike Air Jordan-01',
+		price: '$200.20',
+	},
+	{
+		imgURL: '/images/shoe5.svg',
+		name: 'Nike Air Jordan-10',
+		price: '$210.20',
+	},
+	{
+		imgURL: '/images/shoe6.svg',
+		name: 'Nike Air Jordan-100',
+		price: '$220.20',
+	},
+	{
+		imgURL: '/images/shoe7.svg',
+		name: 'Nike Air Jordan-001',
+		price: '$230.20',
+	},
+]
