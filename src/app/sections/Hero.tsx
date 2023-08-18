1f868526eb7b58188241b17613c4cd30b0dba622
anoopraju31 <anoopraju31@gmail.com>
2023-08-18 20:24:37 +0530

    Hero Section: left side styled

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -8,16 +8,22 @@ const Hero = () => {
 		<section className='xl:padding-l wide:padding-r padding-b'>
 			<div
 				id='home'
-				className='w-full min-h-screen p-2 flex flex-col xl:flex-row gap-10 max-container'>
-				<div className='relative w-full xl:w-2/5 max-xl:padding-x pt-28 flex flex-col justify-center items-center'>
-					<p className=''> Our Summary Collections </p>
-					<h1 className=''>
-						<span> The New Arrival </span>
+				className='w-full min-h-screen p-2 flex flex-col xl:flex-row justify-center gap-10 max-container'>
+				<div className='relative w-full xl:w-2/5 max-xl:padding-x pt-28 flex flex-col justify-center items-start'>
+					<p className='text-xl font-montserrat text-coral-red'>
+						{' '}
+						Our Summary Collections{' '}
+					</p>
+					<h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+						<span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+							{' '}
+							The New Arrival{' '}
+						</span>
 						<br />
-						<span> Nike </span>
+						<span className='text-coral-red inline-block mt-3'> Nike </span>
 						Shoes
 					</h1>
-					<p className=''>
+					<p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
 						Discover stylish Nike arrivals, quality comfort, and innovation for
 						your active life.
 					</p>
@@ -27,8 +33,14 @@ const Hero = () => {
 					<div className='w-full mt-20 flex justify-start items-start flex-wrap gap-16'>
 						{statistics.map((stat, idx) => (
 							<div key={idx} className=''>
-								<p className=''> {stat.value} </p>
-								<p className=''> {stat.label} </p>
+								<p className='text-4xl font-palanquin font-bold'>
+									{' '}
+									{stat.value}{' '}
+								</p>
+								<p className='leading-7 font-montserrat text-slate-gray'>
+									{' '}
+									{stat.label}{' '}
+								</p>
 							</div>
 						))}
 					</div>
