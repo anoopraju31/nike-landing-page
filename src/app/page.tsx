50280f4be84540badf2d12a2c13209ab61bffaeb
anoopraju31 <anoopraju31@gmail.com>
2023-08-18 22:57:26 +0530

    Super Quality Section: intial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
-import Image from 'next/image'
-import { Hero, PopularProducts } from './sections'
+import { Hero, PopularProducts, SuperQuality } from './sections'
 
 export default function Home() {
 	return (
 		<main className=''>
 			{/* Hero Section */}
 			<Hero />
+
 			{/* Popular Products */}
 			<PopularProducts />
+
 			{/* Super Quailty */}
+			<SuperQuality />
+
 			{/* Services */}
 			{/* Special Offers */}
 			{/* Customer Reviews */}
--- src/app/sections/index.ts
+++ src/app/sections/index.ts
@@ -1,2 +1,3 @@
 export { default as Hero } from './Hero'
 export { default as PopularProducts } from './PopularProducts'
+export { default as SuperQuality } from './SuperQuality'
